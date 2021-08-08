046fdeee5a27c0c0f0b1989ac47d37aab2a335bc
bloom <1092429432@qq.com>
2021-08-08 22:09:41 +0800

    初始提交

---
+++ src/main/java/com/bloom/bloommybatis/BloomMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.bloom.bloommybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomMybatisApplication.class, args);
+	}
+
+}
